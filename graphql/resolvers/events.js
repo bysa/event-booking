2d432b9a071accb8e97e858d9d3e8f210c8c20ed
Shafigh <bit_!088>
2019-04-06 06:03:45 -0400

    add auth

--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -1,4 +1,5 @@
 const Event = require("../../models/event");
+const User = require("../../models/user");
 
 const { transformEvent } = require("./merge");
 
@@ -13,19 +14,22 @@ module.exports = {
       throw err;
     }
   },
-  createEvent: async args => {
+  createEvent: async (args, req) => {
+    if (!req.isAuth) {
+      throw new Error("Not authenticated!");
+    }
     const event = new Event({
       title: args.eventInput.title,
       description: args.eventInput.description,
       price: +args.eventInput.price,
       date: new Date(args.eventInput.date),
-      creator: "5ca715035c65654fcc3af936"
+      creator: req.userId
     });
     let createdEvent;
     try {
       const result = await event.save();
       createdEvent = transformEvent(result);
-      const creator = await User.findById("5ca715035c65654fcc3af936");
+      const creator = await User.findById(req.userId);
 
       if (!creator) {
         throw new Error("User not found.");
